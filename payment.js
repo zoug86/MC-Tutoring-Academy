fec465fe821896a603c78ec8215e253b1ace4b41
Houssem Marzougui <marzouguih86@gmail.com>
2021-08-25 12:01:52 -0700

    small fixes

--- payment.js
+++ payment.js
@@ -2,4 +2,4 @@ const selected = localStorage.getItem('selected');
 
 console.log(selected)
 
-const paymentText = document.querySelector('.payment-text').innerHTML = `Your total order is: $${selected}`
+const paymentText = document.querySelector('.payment-text').innerHTML = `Your total is: $${selected}.00`
